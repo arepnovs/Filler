5aae762c495e718453654288da5931ee2981075c
Anton Repnovsky <repnovant@gmail.com>
2017-03-02 15:31:00 +0200

    Added some comments

--- filler.h
+++ filler.h
@@ -10,37 +10,37 @@
 # define GREEN  "\x1B[32m"
 # define RESET  "\x1B[0m"
 
-typedef struct		s_cd
+typedef struct			s_cd
 {
-	int				x;
-	int				y;
-	int 			i;
-	int				*px;
-	int				*py;
-}					t_cd;
+	int			x;//size x
+	int			y;//size y
+	int 			i;//amount of dots/symbols/ets
+	int			*px;//coordinate x of dots/symbols/ets
+	int			*py;//coordinate y of dots/symbols/ets
+}				t_cd;
 
-typedef struct		s_game
+typedef struct			s_game
 {
-	char			sign;
-	char			**p_symb;
-	char			**m_symb;
-	char			**map_cmp;
-	int				flag;
+	char			sign;//player 1 or 2
+	char			**p_symb;//piece
+	char			**m_symb;//map
+	char			**map_cmp;//previous map to compare with current(bot didnt receive downcase letters of last move)
+	int			flag;
 	t_cd			map;
 	t_cd			piece;
-	t_cd			p1;
-	t_cd			p2;
-	t_cd			all_ok;
-	t_cd			t;
-	t_cd			e_move;
-}					t_game;
+	t_cd			p1;//player 1
+	t_cd			p2;//player 1
+	t_cd			all_ok;//all posibble coordinates to place the piece
+	t_cd			t;//temp coordinates of piece on a map
+	t_cd			e_move;//coordinates of enemy's last move 
+}				t_game;
 
 /*arepnovs_filler.c*/
 void		get_piece(t_game *game, char map[game->map.y][game->map.x]);
 void		get_map(t_game *game);
 void		use_strategy(t_game *game);
 void		game_start(t_game *game);
-int			main(void);
+int		main(void);
 /*read.c*/
 void		read_piece(t_game *game, char piece[game->piece.y][game->piece.x]);
 void		remember_piece(t_game *game, char piece[game->piece.y][game->piece.x]);
@@ -48,13 +48,13 @@ void		read_o_map(t_game *game, char map[game->map.y][game->map.x]);
 void		read_x_map(t_game *game, char map[game->map.y][game->map.x]);
 void		remember_map(t_game *game, char map[game->map.y][game->map.x]);
 /*player1.c*/
-int			make_check_o(t_game *g, int ty, int tx, int i);
-int			check_o(t_game *game, int tx, int ty);
+int		make_check_o(t_game *g, int ty, int tx, int i);
+int		check_o(t_game *game, int tx, int ty);
 t_game		*all_o(t_game *game, int x, int y, int *i);
 /*player2.c*/
 void		map_copy(t_game *game);
-int			make_check_x(t_game *g, int ty, int tx, int i);
-int			check_x(t_game *game, int tx, int ty);
+int		make_check_x(t_game *g, int ty, int tx, int i);
+int		check_x(t_game *game, int tx, int ty);
 t_game		*all_x(t_game *game, int x, int y, int *i);
 /*tools.c*/
 t_cd		set_coord(int x, int y);
@@ -70,6 +70,6 @@ void		millitary_secret(t_game *game, int j, int i);
 t_game		*aggressive(t_game *gm);
 /*bonus*/
 int		get_res(char *line);
-void	input_check(char *line);
+void		input_check(char *line);
 
 #endif
